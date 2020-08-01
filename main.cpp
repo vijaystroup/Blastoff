a87c1c2113631fe712507d7a4d826237f9cb04f9
Vijay Stroup <vijaystroup@gmail.com>
2020-08-01 11:48:20 -0400

    sensor name setter/getter and to_string

--- main.cpp
+++ main.cpp
@@ -8,15 +8,27 @@ using std::string;
 class sensor {
 private:
     string name;
+    double data;
 public:
-    sensor(string name) {
+    sensor(string name, int min, int max) {
         this->name = name;
+        // gen_data(min, max);
     };
+
+    string get_name() {return this->name;}
+    void set_name(string name) {this->name = name;}
+
+    string to_string() {
+        return "sensor " + get_name() + ": ";
+    }
 };
 
 int main(void) {
     srand(time(NULL));
     print("{}\n", rand() % 100);
+
+    sensor s("boom");
+    print("{}\n", s.to_string());
 
     return 0;
 }
