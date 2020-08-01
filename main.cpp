433920dae2a92272ba64577f8f019c27b51442ed
Vijay Stroup <vijaystroup@gmail.com>
2020-08-01 13:23:28 -0400

    formatting

--- main.cpp
+++ main.cpp
@@ -10,10 +10,9 @@ class sensor {
 private:
     string name;
     int data;
-
-public:
     int min, max;
 
+public:
     sensor(string name, int min, int max) {
         set_name(name);
         this->data = -1;
@@ -22,8 +21,8 @@ public:
     };
 
     // name
-    string get_name() {return this->name;}
-    void set_name(string name) {this->name = name;}
+    string get_name() { return this->name; }
+    void set_name(string name) { this->name = name; }
 
     // data
     int get_data() {return this->data;}
@@ -31,7 +30,9 @@ public:
         this->data = this->min + (rand() % (this->max - this->min + 1));
     }
 
-    string to_string() {return "sensor " + get_name() + ": " + std::to_string(get_data());}
+    string to_string() {
+        return "sensor " + get_name() + ": " + std::to_string(get_data());
+    }
 };
 
 int main(void) {
