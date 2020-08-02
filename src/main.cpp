e336d0ab1ab0bdcf9c536392b2f48d27157f63da
Vijay Stroup <vijaystroup@gmail.com>
2020-08-02 17:50:45 -0400

    sleep for data stream

--- src/main.cpp
+++ src/main.cpp
@@ -9,10 +9,10 @@ int main(void) {
     srand(time(NULL));
 
     sensor s("boom", 10, 50);
-    for (int i = 0; i < 10; i++) {
+    while(true) {
         s.set_data();
         print("{}\n", s.to_string());
-        sleep(1);
+        usleep(100000);
     }
 
     return 0;
