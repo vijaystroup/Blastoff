39842035875c750e49f5b174fec50fc3f70a286d
Vijay Stroup <vijaystroup@gmail.com>
2020-08-05 14:08:02 -0400

    using glade

--- src/main.cc
+++ src/main.cc
@@ -1,24 +1,38 @@
-#include <gtkmm/application.h>
-#include <gtkmm/window.h>
-#include "accelerometer_gui.h"
-#include "velocimeter_gui.h"
+#include <gtkmm.h>
+#include <string.h>
 
+using std::string;
+
+
+void on_button_clicked(Gtk::Button* pButton) {
+    printf("hehe\n");
+    pButton->hide();
+}
 
 int main(int argc, char *argv[]) {
+    Gtk::Window* win;
+    
+    // create application
     auto app = Gtk::Application::create(argc, argv, "com.vijaystroup.BlastOff");
 
-    Gtk::Window win;
-    win.set_title("BlastOff");
+    // get the refrence builder
+    string glade_file = "src/gui/outline.glade";
+    auto refBuilder = Gtk::Builder::create();
+    refBuilder->add_from_file(glade_file);
 
-    Accelerometer_Gui acc;
-    Velocimeter_Gui vel;
-    
-    
+    // add the glade window and widgets
+    refBuilder->get_widget("window", win);
+    if (win) {
+        // setup gui
+        Gtk::Button *button_launch = nullptr;
+        refBuilder->get_widget("button_launch", button_launch);
+        if (button_launch) {
+            button_launch->signal_clicked().connect(sigc::bind(sigc::ptr_fun(&on_button_clicked), button_launch));
+        }
 
-    win.add(acc);
-    acc.show();
-    win.add(vel);
-    vel.show();
+        app->run(*win);
+    }
 
-    return app->run(win);
+    delete win;
+    return 0;
 }
